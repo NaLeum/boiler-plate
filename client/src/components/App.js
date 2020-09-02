a48536e4cc34f55dee7f54cc365a01ba8a3d4032
NaLeum <wognsdl@likelion.org>
2020-09-02 22:54:13 +0900

    add Footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,23 +7,34 @@ import {
 import LandingPage from './views/LandingPage/LandingPage';
 import LoginPage from './views/LoginPage/LoginPage';
 import RegisterPage from './views/RegisterPage/RegisterPage';
+import Footer from './views/Footer/Footer';
+import NavBar from './views/NavBar/NavBar';
 import Auth from '../hoc/auth'
 
 
+    // null => 아무나 출입이 가능한 페이지
+    // true => 로그인한 유저만 출입이 가능한 페이지
+    // false => 로그인한 유저는 출입 불가능한 페이지
+
 const App = () => {
   return (
-    <Router>
-    <div>
+    <Suspense fallback={(<div>Loading</div>)}>
+      <Router>
+        <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
 
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-        <Route exact path="/" component={Auth(LandingPage,null)} />
-        <Route exact path="/login" component={Auth(LoginPage,false)}/>
-        <Route exact path="/register" component={Auth(RegisterPage,false)}/>
-      </Switch>
-    </div>
-  </Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage,null)} />
+          <Route exact path="/login" component={Auth(LoginPage,false)}/>
+          <Route exact path="/register" component={Auth(RegisterPage,false)}/>
+        </Switch>
+      </div>
+    </Router>
+    <Footer />
+  </Suspense>
   );
 }
 
